fcb51881c01f1569f35a14d2d1937bbb9c0f5299
probin-dhakal <154975813+probin-dhakal@users.noreply.github.com>
2025-08-08 18:49:43 +0530

    Update Navbar.jsx

--- frontend/resume-builder/src/components/layouts/Navbar.jsx
+++ frontend/resume-builder/src/components/layouts/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
       <div className="container mx-auto flex items-center justify-between gap-5">
         <Link to='/dashboard'>
           <h2 className="text-lg md:text-xl font-medium text-black leading-5">
-            Resume Builder
+            JobFitPro
           </h2>
         </Link>
 
